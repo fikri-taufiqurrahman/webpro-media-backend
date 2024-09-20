4abddbffe3ed1667f72655bb2741b378601e4a01
fikri-taufiqurrahman <fikritaufiqurrahman7@gmail.com>
2024-09-20 23:03:25 +0700

    Recreate realtime chat
    Co-authored-by: Gaduh Hartawan gaduhhartawan412@gmail.com

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -4,9 +4,9 @@ import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/chat", authMiddleware, sendMessage);
+// router.post("/chat", authMiddleware, sendMessage);
 
 // Route untuk mendapatkan semua pesan antara dua pengguna
-router.get("/chat/:receiverId", authMiddleware, getMessages);
+// router.get("/chat/:receiverId", authMiddleware, getMessages);
 
 export default router;
