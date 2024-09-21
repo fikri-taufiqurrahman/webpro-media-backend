4c25890b09111370dc3eedcfd8aff46c38912915
gaduhhartawan <garzent12@gmail.com>
2024-09-21 08:23:10 +0700

    feat: update sequelize config

--- config/db.js
+++ config/db.js
@@ -11,7 +11,7 @@ export const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "mysql",
-    dialectModule: "mysql2",
+    dialectModule: require("mysql2"),
     pool: {
       max: 5,
       min: 0,
